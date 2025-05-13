0585537caebc7f6fda531f45c7f0c7acf2f599b8
Menuoo <lietuvis7080@gmail.com>
2025-05-13 15:45:27 +0300

    Rendering 12 complete

--- Assets/Editor/MyLightingShaderGUI.cs
+++ Assets/Editor/MyLightingShaderGUI.cs
@@ -225,7 +225,30 @@ public class MyLightingShaderGUI : ShaderGUI {
                 m.SetInt("_ZWrite", settings.zWrite ? 1 : 0);
             }
         }
+
+        if (mode == RenderingMode.Fade || mode == RenderingMode.Transparent)
+        {
+            DoSemitransparentShadows();
+        }
     
+    }
+
+    void DoSemitransparentShadows()
+    {
+        EditorGUI.BeginChangeCheck();
+        bool semitransparentShadows = EditorGUILayout.Toggle(
+            MakeLabel("Semi Shadows", "Semitransparent Shadows"),
+            IsKeywordEnabled("_SEMITRANSPARENT_SHADOWS")
+        );
+        if (EditorGUI.EndChangeCheck())
+        {
+            SetKeyword("_SEMITRANSPARENT_SHADOWS", semitransparentShadows);
+        }
+
+        if (!semitransparentShadows)
+        {
+            shouldShowAlphaCutoff = true;
+        }
     }
 
 
